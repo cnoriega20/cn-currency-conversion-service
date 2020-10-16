f5b2b0c5b18698c77a84ec2450c8629c9fc1baf7
cnoriega20 <c79.noriega@gmail.com>
2020-10-15 21:15:20 -0500

    Invoked Currency Exchange service.

--- src/main/java/tn/mscvs/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/tn/mscvs/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,27 +1,43 @@
 package tn.mscvs.currencyconversionservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import tn.mscvs.currencyconversionservice.domain.CurrencyConversion;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+    private RestTemplate restTemplate;
+
+    private final String URI = "http://localhost:8000/currency-exchange/get?from={from}&to={to}";
 
     @GetMapping("/convert-currency/get")
     public CurrencyConversion convert(@RequestParam("from") String from,
                                       @RequestParam("to") String to,
                                       @RequestParam("amount")BigDecimal amount){
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity(URI, CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
         return CurrencyConversion.builder()
-                .id(1L)
+                .id(currencyConversion.getId())
                 .from(from)
                 .to(to)
-                .conversionValue(BigDecimal.TEN)
+                .conversionValue(currencyConversion.getConversionValue())
                 .amount(amount)
-                .totalAmount(amount)
-                .port(0)
+                .totalAmount(amount.multiply(currencyConversion.getConversionValue()))
+                .port(currencyConversion.getPort())
                 .build();
     }
 
