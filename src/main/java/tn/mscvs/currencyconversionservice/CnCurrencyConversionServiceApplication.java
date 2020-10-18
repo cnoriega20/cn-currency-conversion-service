579c3c0229f63ea9872298ca2c55e0382e3959d2
cnoriega20 <c79.noriega@gmail.com>
2020-10-18 14:38:45 -0500

    Changed feign dependency.

--- src/main/java/tn/mscvs/currencyconversionservice/CnCurrencyConversionServiceApplication.java
+++ src/main/java/tn/mscvs/currencyconversionservice/CnCurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package tn.mscvs.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CnCurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
