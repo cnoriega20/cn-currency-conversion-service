46929057b5bbfa2f3e0812ca7ae23f175530afdd
cnoriega20 <c79.noriega@gmail.com>
2020-10-19 14:42:16 -0500

    Adding Ribbon configuration.

--- src/main/java/tn/mscvs/currencyconversionservice/services/CurrencyExchangeServiceProxy.java
+++ src/main/java/tn/mscvs/currencyconversionservice/services/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,14 @@
 package tn.mscvs.currencyconversionservice.services;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tn.mscvs.currencyconversionservice.domain.Exchange;
 
-@FeignClient(name = "cn-currency-exchange-service", url = "http://localhost:8000")
+//@FeignClient(name = "cn-currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "cn-currency-exchange-service")
+@RibbonClient(name = "cn-currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/get")
