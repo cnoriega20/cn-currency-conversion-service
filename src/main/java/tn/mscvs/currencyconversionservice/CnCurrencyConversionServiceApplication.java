1712fce1a3556a169d5ccfc3f830df5f852a06cb
cnoriega20 <c79.noriega@gmail.com>
2020-10-20 02:30:41 -0500

    Register application to Eureka Naming Server

--- src/main/java/tn/mscvs/currencyconversionservice/CnCurrencyConversionServiceApplication.java
+++ src/main/java/tn/mscvs/currencyconversionservice/CnCurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package tn.mscvs.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CnCurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
